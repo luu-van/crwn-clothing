ef32a46cbf77cb05081aef552fa0cf390bfb746f
Luu Van <vanngocluu94@gmail.com>
2021-08-27 17:13:53 +0700

    shop collection data

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,11 @@
+import './collection-preview.styles.scss'
+
+
+const CollectionPreview = () => (
+    <div className='collection-preview'>
+        <h1 className='title'>Title</h1>
+        <div className='preview'>
+
+        </div>
+    </div>
+)
