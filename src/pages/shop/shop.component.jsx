5a53ecd8079ba8a257de01e33b8f352440b27ac2
Luu Van <vanngocluu94@gmail.com>
2021-08-28 21:56:53 +0700

    add collection item and shop data

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,18 +1,28 @@
-import React from "react";
-import SHOP_DATA from "./shop.data";
+import React from 'react';
+
+import SHOP_DATA from './shop.data.js';
+
+import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx';
 
 class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            collection: SHOP_DATA
-        }
-    }
+    this.state = {
+      collections: SHOP_DATA
+    };
+  }
 
-    render(){
-        return <div>SHOP PAGE</div>;
-    }
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className='shop-page'>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default ShopPage;
