56bf314394283eba80402d97c4fa64a829715574
Luu Van <vanngocluu94@gmail.com>
2021-08-31 17:40:32 +0700

    form sign in

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import { Switch, Route } from 'react-router';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-up/sign-in-up.component';
+import Header from './components/header/header.component';
 
 function App() {
   return <div>
+    <Header />
     <Switch>
       <Route exact path='/' component ={HomePage} />
       <Route path='/shop' component ={ShopPage} />
+      <Route path='/signin' component ={SignInAndSignUpPage} />
     </Switch>
   </div>;
 }
---
+++ src/pages/sign-in-up/sign-in-up.component.jsx
@@ -0,0 +1,11 @@
+import './sign-in-up.styles.scss';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const SignInAndSignUpPage = () => (
+    <div className='sign-in-up'>
+        <SignIn />
+    </div>
+)
+
+
+export default SignInAndSignUpPage;
