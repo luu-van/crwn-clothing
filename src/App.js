0c0099d0a8acedb73b5676a023b607166cb78de5
Luu Van <vanngocluu94@gmail.com>
2021-09-04 17:02:53 +0700

    add button sign in to header

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Switch, Route } from 'react-router';
 
 import './App.css';
@@ -5,16 +6,41 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-up/sign-in-up.component';
 import Header from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return <div>
-    <Header />
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return <div>
+    <Header currentUser={this.state.currentUser} />
     <Switch>
       <Route exact path='/' component ={HomePage} />
       <Route path='/shop' component ={ShopPage} />
       <Route path='/signin' component ={SignInAndSignUpPage} />
     </Switch>
   </div>;
+  }
 }
 
 export default App;
