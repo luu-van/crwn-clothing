8a0f43fa4901fc4aed27b3625b16465b2d9532ed
Luu Van <vanngocluu94@gmail.com>
2021-09-20 16:20:20 +0700

    store

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils'
 import './header.styles.scss';
@@ -26,4 +27,8 @@ const Header = ({ currentUser }) => (
     </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
