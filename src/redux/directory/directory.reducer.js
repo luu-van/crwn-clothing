74471b4184378fd23a74383f712531aafc6bafeb
Luu Van <vanngocluu94@gmail.com>
2021-10-10 10:18:22 +0700

    data flow

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -1,28 +1,28 @@
 const INITIAL_STATE = {
     sections: [{
-        title: 'hats',
-        imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-        id: 1,
-        linkUrl: 'hats'
-    },
+            title: 'hats',
+            imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+            id: 1,
+            linkUrl: 'shop/hats'
+        },
         {
             title: 'jackets',
             imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
             id: 2,
-            linkUrl: ''
+            linkUrl: 'shop/jackets'
         },
         {
             title: 'sneakers',
             imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
             id: 3,
-            linkUrl: ''
+            linkUrl: 'shop/sneakers'
         },
         {
             title: 'womens',
             imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
             size: 'large',
             id: 4,
-            linkUrl: ''
+            linkUrl: 'shop/womens'
 
         },
         {
@@ -30,7 +30,7 @@ const INITIAL_STATE = {
             imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
             size: 'large',
             id: 5,
-            linkUrl: ''
+            linkUrl: 'shop/mens'
 
         }
     ]
